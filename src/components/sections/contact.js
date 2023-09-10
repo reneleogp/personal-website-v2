0071e373308508b3753db8d7dab04c93d68eca15
Rene Gonzalez <reneleogp0305@gmail.com>
2023-09-10 01:16:15 -0400

    contact updated

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,12 +60,12 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        My inbox is always open! Whether you have job opportunities to discuss or just want to say
+        hello, don't hesitate to reach out.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
-        Say Hello
+        Contact me
       </a>
     </StyledContactSection>
   );
