65684768bca28f1464b2e098ca77cd0291ab082b
Rene Gonzalez <reneleogp0305@gmail.com>
2024-05-07 17:55:24 -0400

    some changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,14 +4,14 @@ import styled from 'styled-components';
 import { Icon } from '@components/icons';
 import { socialMedia } from '@config';
 
-const StyledFooter = styled.footer`
-  ${({ theme }) => theme.mixins.flexCenter};
-  flex-direction: column;
-  height: auto;
-  min-height: 70px;
-  padding: 15px;
-  text-align: center;
-`;
+// const StyledFooter = styled.footer`
+//   ${({ theme }) => theme.mixins.flexCenter};
+//   flex-direction: column;
+//   height: auto;
+//   min-height: 70px;
+//   padding: 15px;
+//   text-align: center;
+// `;
 
 const StyledSocialLinks = styled.div`
   display: none;
@@ -40,32 +40,32 @@ const StyledSocialLinks = styled.div`
   }
 `;
 
-const StyledCredit = styled.div`
-  color: var(--light-slate);
-  font-family: var(--font-mono);
-  font-size: var(--fz-xxs);
-  line-height: 1;
+// const StyledCredit = styled.div`
+//   color: var(--light-slate);
+//   font-family: var(--font-mono);
+//   font-size: var(--fz-xxs);
+//   line-height: 1;
 
-  a {
-    padding: 10px;
-  }
+//   a {
+//     padding: 10px;
+//   }
 
-  .github-stats {
-    margin-top: 10px;
+//   .github-stats {
+//     margin-top: 10px;
 
-    & > span {
-      display: inline-flex;
-      align-items: center;
-      margin: 0 7px;
-    }
-    svg {
-      display: inline-block;
-      margin-right: 5px;
-      width: 14px;
-      height: 14px;
-    }
-  }
-`;
+//     & > span {
+//       display: inline-flex;
+//       align-items: center;
+//       margin: 0 7px;
+//     }
+//     svg {
+//       display: inline-block;
+//       margin-right: 5px;
+//       width: 14px;
+//       height: 14px;
+//     }
+//   }
+// `;
 
 const Footer = () => {
   const [setGitHubInfo] = useState({
@@ -90,40 +90,18 @@ const Footer = () => {
   }, []);
 
   return (
-    <StyledFooter>
-      <StyledSocialLinks>
-        <ul>
-          {socialMedia &&
-            socialMedia.map(({ name, url }, i) => (
-              <li key={i}>
-                <a href={url} aria-label={name}>
-                  <Icon name={name} />
-                </a>
-              </li>
-            ))}
-        </ul>
-      </StyledSocialLinks>
-
-      <StyledCredit tabindex="-1">
-        <a href="https://github.com/reneleogp/personal-website-v2">
-          <div>Original design by Brittany Chiang</div>
-          <br />
-          <div>Revised by Rene Gonzalez</div>
-          {/* {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )} */}
-        </a>
-      </StyledCredit>
-    </StyledFooter>
+    <StyledSocialLinks>
+      <ul>
+        {socialMedia &&
+          socialMedia.map(({ name, url }, i) => (
+            <li key={i}>
+              <a href={url} aria-label={name}>
+                <Icon name={name} />
+              </a>
+            </li>
+          ))}
+      </ul>
+    </StyledSocialLinks>
   );
 };
 
